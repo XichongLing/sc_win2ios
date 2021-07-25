4039fb99d1dad1be57e1901c2662f78482b14c5c
XichongLing <xichong.ling@mail.mcgill.ca>
2021-07-25 18:49:06 +0800

    test version

---
+++ monitor_ps.ps1
@@ -0,0 +1,11 @@
+﻿$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.IncludeSubdirectories = $false
+$watcher.Path = 'C:\Users\Dell\Pictures\Screenshots'
+$watcher.EnableRaisingEvents = $true
+$action =
+{
+    $path = $event.SourceEventArgs.FullPath
+    $changetype = $event.SourceEventArgs.ChangeType
+    Write-Host "$path was $changetype at $(get-date)"
+}
+Register-ObjectEvent $watcher "Created" -Action $action
