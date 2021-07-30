a7877bd9b0b9d47838394390454ca0ea1c6e5dc0
XichongLing <xichong.ling@mail.mcgill.ca>
2021-07-30 23:22:37 +0800

    copy latest screenshots to target folder

--- monitor_ps.ps1
+++ monitor_ps.ps1
@@ -1,14 +1,20 @@
-﻿$watcher = New-Object System.IO.FileSystemWatcher
+﻿#subscribe to an event listener
+$watcher = New-Object System.IO.FileSystemWatcher
 $watcher.IncludeSubdirectories = $false
 
-$userName = $(whoami)
-$targetPath = 'C:\Users\'+$userName+'\Pictures\Screenshots'
+#specify target folder
+$targetPath = 'C:\Users\Dell\Pictures\Screenshots'
 $watcher.Path = $targetPath
 $watcher.EnableRaisingEvents = $true
+
+#specify actions to take when target event triggered
 $action =
 {
-    $path = $event.SourceEventArgs.FullPath
-    $changetype = $event.SourceEventArgs.ChangeType
-    Write-Host "$path was $changetype at $(get-date)"
+    $file=gci 'C:\Users\Dell\Pictures\Screenshots' | sort LastWriteTime | select -last 1 |select -expand Fullname
+    cp $file C:\Users\Dell\Desktop\ceshi
 }
+
+#initiate the listener
 Register-ObjectEvent $watcher "Created" -Action $action
+
+#get-eventsubscriber -force | unregister-event -force
