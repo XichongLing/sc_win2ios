01bfde5d16b5f56abd9d2495e89a043a25f73690
XichongLing <xichong.ling@mail.mcgill.ca>
2021-07-31 17:47:33 +0800

    create a transfer folder

--- monitor_ps.ps1
+++ monitor_ps.ps1
@@ -1,4 +1,9 @@
-﻿#subscribe to an event listener
+﻿#build a transfer folder on desktop
+$transferFolderName = Read-Host 'build a transfer folder on Desktop'
+$transferFolder = $env:USERPROFILE + '\Desktop\' + $transferFolderName
+New-Item -ItemType directory -Path $transferFolder
+
+#subscribe to an event listener
 $watcher = New-Object System.IO.FileSystemWatcher
 $watcher.IncludeSubdirectories = $false
 
@@ -7,14 +12,17 @@ $targetPath = $env:USERPROFILE +'\Pictures\Screenshots'
 $watcher.Path = $targetPath
 $watcher.EnableRaisingEvents = $true
 
+
 #specify actions to take when target event triggered
 $action =
 {
     $file=gci 'C:\Users\Dell\Pictures\Screenshots' | sort LastWriteTime | select -last 1 |select -expand Fullname
-    cp $file C:\Users\Dell\Desktop\ceshi
+    cp $file $transferFolder
 }
 
 #initiate the listener
 Register-ObjectEvent $watcher "Created" -Action $action
+
+
 
 #get-eventsubscriber -force | unregister-event -force
